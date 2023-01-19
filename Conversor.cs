af7a154e7aab651f9367881c46cedfab2e04f730
Evandru <evandro_remolli.t@outlook.com>
2023-01-19 14:02:14 -0300

    First commit of DollarsToCents

---
+++ Conversor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DollarsToCents
+{
+    public static class Conversor
+    {
+        public static string ToDollar(int cents)
+        {
+            string centsString = cents.ToString();
+            string dollar = "$" + centsString.Insert(centsString.Length-2, ".");
+            return dollar;
+        }
+        
+        public static int ToCents(string value)
+        {
+            string dollarsFormat = value.Replace("$", "");
+            string dollars = dollarsFormat.Replace(".", "");
+            int cents = Convert.ToInt32(dollars);
+            return cents;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using DollarsToCents;
+
+Console.WriteLine("Conversor");
+Console.WriteLine();
+Console.WriteLine("'1' Dollars para Cents");
+Console.WriteLine("'2' Cents para Dollars");
+
+string escolha = Console.ReadLine();
+Console.Clear();
+
+if (escolha == "1")
+{
+    Console.WriteLine("Dollars para Cents");
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Tipo de formatação: $123.45");
+    Console.ResetColor();
+    Console.WriteLine();
+    Console.Write("Insira a quantidade em Dollars: ");
+    string dollars = Console.ReadLine();
+    Console.WriteLine("Cents: " + Conversor.ToCents(dollars));
+}
+else if(escolha == "2")
+{
+    Console.WriteLine("Cents para Dollars");
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Tipo de formatação: 12345");
+    Console.ResetColor();
+    Console.WriteLine();
+    Console.Write("Insira a quantidade em Cents: ");
+    int cents = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Cents: " + Conversor.ToDollar(cents));
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Essa escolha não existe.");
+}
